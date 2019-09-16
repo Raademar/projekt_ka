28f1264081c2f54d70d93efac6b27a40415cf965
Izabella <izzwii.izabella@gmail.com>
2019-09-16 19:10:28 +0200

    setup basic routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
+import { Switch, Route } from 'react-router'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import theme from './style/theme'
 import GlobalStyle from './style/global'
 import Start from './views/Start'
-import CategoryFilter from './components/CategoryFilter'
-import FeaturedClipsList from './components/FeaturedClipsList'
 import About from './views/About'
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <>
-        <GlobalStyle />
-        {/* <Start />
-        <CategoryFilter />
-        <FeaturedClipsList /> */}
-        <About></About>
-      </>
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <>
+          <GlobalStyle />
+          <Switch>
+            <Route exact path="/" component={Start}></Route>
+            <Route path="/about" component={About}></Route>
+          </Switch>
+        </>
+      </ThemeProvider>
+    </Router>
   )
 }
 
--- src/views/Start.js
+++ src/views/Start.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import Searchbar from '../components/Searchbar'
-
+import CategoryFilter from '../components/CategoryFilter'
+import FeaturedClipsList from '../components/FeaturedClipsList'
 const Start = () => {
   return (
     <div>
       <Nav></Nav>
       <Searchbar></Searchbar>
+      <CategoryFilter />
+      <FeaturedClipsList />
     </div>
   )
 }
