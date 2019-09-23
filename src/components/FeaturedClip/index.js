3af7962050fc437ed3a631a5a7825723fd7f7866
Izabella <izzwii.izabella@gmail.com>
2019-09-23 14:32:24 +0200

    short title in slider

--- src/components/FeaturedClip/index.js
+++ src/components/FeaturedClip/index.js
@@ -7,7 +7,7 @@ const FeaturedClip = props => {
     <StyledFeaturedClip>
       <NavLink to={props.link}>
         <img src={props.image} alt="thumbnail_from_video" />
-        <p>{props.text}</p>
+        <p>{props.text.substr(0, 50)}...</p>
       </NavLink>
     </StyledFeaturedClip>
   )
