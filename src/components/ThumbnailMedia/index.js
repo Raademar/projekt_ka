4b6f7c681c9cbed3c6753ec8b80482736f686480
Izabella <izzwii.izabella@gmail.com>
2019-09-23 11:46:54 +0200

    changed some data

--- src/components/ThumbnailMedia/index.js
+++ src/components/ThumbnailMedia/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { StyledThumbnailMedia } from './style'
 const ThumbnailMedia = props => {
+  const { content } = props
+  console.log(content)
+
   return (
     <StyledThumbnailMedia color={props.color}>
       <img src="https://images.unsplash.com/photo-1562185022-c0a7889d7fbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80" />
