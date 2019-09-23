461b19c8259112a9163af231322fc217cee17035
Izabella <izzwii.izabella@gmail.com>
2019-09-23 09:14:58 +0200

    trying to scroll to top on refresh

--- src/components/Layout/style.js
+++ src/components/Layout/style.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 export const Wrapper = styled.div`
-  /* margin: 0 20px; */
-  margin: 0 ${props => (props.noMargin ? '0' : '20px')};
+  /* margin: 70px 20px 0 20px; */
+  margin: 70px ${props => (props.noMargin ? '0' : '20px')} 0
+    ${props => (props.noMargin ? '0' : '20px')};
   h3 {
     text-transform: capitalize;
   }
