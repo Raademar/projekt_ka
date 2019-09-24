0153437aa259b71006fe0a9f24d5aae030a3f271
Izabella <izzwii.izabella@gmail.com>
2019-09-24 10:47:02 +0200

    started with filtering search

--- src/views/SearchResult.js
+++ src/views/SearchResult.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import Footer from '../components/Footer'
 import SrcResultsFilter from '../components/SrcResultsFilter'
 import Header from '../components/Header'
 import Layout from '../components/Layout'
 import Filter from '../components/Filter'
+import { DataContext } from '../App'
+
 const SearchResult = props => {
-  console.log(props)
+  const { location } = props
+  const { data, setData } = useContext(DataContext)
+  const searchWord = location.search.split('?')
+
+  // const searchQuery = data => {
+  //   return data.filter(
+  //     m => m.title.toLowerCase().indexOf(searchWord[0].toLowerCase()) >= 0
+  //   )
+  // }
+  const searchQuery = data => {
+    return data.filter(m =>
+      console.log(
+        m.title.toLowerCase().indexOf(searchWord[1].toLowerCase()) >= 0
+      )
+    )
+  }
+
+  console.log(searchQuery(data))
+  // const filter = data.map(item => item.title.search(searchWord[1]))
+  // const filterSearch = data.find(item => item.title === searchWord[1])
+  // console.log(filterSearch)
+
   return (
     <>
       <Layout history={props.history} noMargin="true">
